95c64091fd00300441df4c34ab69dc02d7648ae2
Jérôme Cuq <jeromelist@gmail.com>
2018-06-14 17:08:41 +0200

    code changes for kibana 6 compatibility. Not tested yet, and likely not working yet

--- public/kibi_timeline_vis.js
+++ public/kibi_timeline_vis.js
@@ -1,43 +1,59 @@
-import { VisVisTypeProvider } from 'ui/vis/vis_type';
-import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
-import { VisSchemasProvider } from 'ui/vis/schemas';
+import { CATEGORY } from 'ui/vis/vis_category';
+import { VisFactoryProvider } from 'ui/vis/vis_factory';
+import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 
-import 'plugins/kibi_timeline_vis/kibi_timeline_vis.less';
-import 'plugins/kibi_timeline_vis/kibi_timeline_vis_controller';
-import 'plugins/kibi_timeline_vis/kibi_timeline_vis_params';
-import template from 'plugins/kibi_timeline_vis/kibi_timeline_vis.html';
+import './kibi_timeline_vis.less';
+import './kibi_timeline_vis_controller';
+import optionsTemplate from './kibi_timeline_vis_params.html';
+import template from './kibi_timeline_vis.html';
+
+// register the provider with the visTypes registry
+VisTypesRegistryProvider.register(KibiTimelineVisProvider);
 
 function KibiTimelineVisProvider(Private) {
-  const VisType = Private(VisVisTypeProvider);
-  const TemplateVisType = Private(TemplateVisTypeProvider);
+  const VisFactory = Private(VisFactoryProvider);
   const Schemas = Private(VisSchemasProvider);
 
   // return the visType object, which kibana will use to display and configure new
   // Vis object of this type.
-  return new TemplateVisType({
+  return VisFactory.createAngularVisualization({
     name: 'kibi_timeline',
     title: 'Kibi Timeline',
     icon: 'fak-timeline',
-    category: VisType.CATEGORY.KIBI,
+    category: CATEGORY.KIBI,
     description: 'Timeline widget for visualization of events',
     template,
-    params: {
+    visConfig: {
       defaults: {
-        groups: [],
-        groupsOnSeparateLevels: false,
-        selectValue: 'id',
-        notifyDataErrors: false,
-        syncTime: false
+        //config: {
+          // each group contains { id:_, groupLabel:_, startField:_, endField:_, size:_, orderBy:_ }
+          groups: [{ id:1, groupLabel:'test', startField:'0' }],
+          groupsOnSeparateLevels: false,
+          selectValue: 'id',
+          notifyDataErrors: false,
+          syncTime: false
+        //}
       },
-      editor: '<kibi-timeline-vis-params></kibi-timeline-vis-params>'
+      template: template
     },
+    editorConfig: {
+      optionsTemplate,
+      // schemas: new Schemas([
+      //   {
+      //   },
+      //   {
+      //   }
+      // ])
+    },
+    responseHandler: 'none',
     defaultSection: 'options',
     requiresSearch: false,
     requiresMultiSearch: true,
     requiresTimePicker: true,
-    delegateSearch: true
+    delegateSearch: true,
   });
 }
 
-VisTypesRegistryProvider.register(KibiTimelineVisProvider);
+// export the provider so that the visType can be required with Private()
+export default KibiTimelineVisProvider;
